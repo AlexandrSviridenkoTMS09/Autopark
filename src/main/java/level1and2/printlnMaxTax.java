6ced62c084f8131dc6c4ddd0401025be2ea7011b
AlexandrSviridenkoTMS09 <91564222+AlexandrSviridenkoTMS09@users.noreply.github.com>
2022-05-14 16:08:14 +0300

    Delete printlnMaxTax.java
    Объединен

--- src/main/java/level1and2/printlnMaxTax.java
+++
@@ -1,9 +0,0 @@
-package level1and2;
-
-import static level1and2.Main2.*;
-
-public class printlnMaxTax {
-    public static void printlnMaxTax(VehicleType[] vehicleTypes){
-        System.out.println("maxTax = " + calculationMaxTax(vehicleTypes) + "\n" + "average = " + calculationAverage(vehicleTypes));
-    }
-}
